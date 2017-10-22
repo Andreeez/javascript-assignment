e7b882ac2204de14d04d47f29d3fc2f0e0c021fe
Andreeez <32762098+Andreeez@users.noreply.github.com>
2017-10-22 15:52:08 +0200

    Updating - First response on the website. Products are showed at the site.

--- script.js
+++ script.js
@@ -17,9 +17,66 @@ function createUIFromLoadedProducts() {
     /* Check your console to see that the products are stored in the listOfProducts varible */
     console.log(listOfProducts);
 
-var main = document.querySelector("#main");
+//var main = document.querySelector("#main");
+var main = document.getElementById("main");
 
-main.innerHTML ="Text"
+var allProducts = document.createElement("div");
+
+allProducts.className = "allProductsClass";
+
+for(var index=0; index <listOfProducts.length; index++) {
+
+    var productCard = createproductCard(listOfProducts[index]);
+    allProducts.appendChild(productCard);
+
+} 
+
+document.body.appendChild(allProducts);
+
+
+
+
+
+
+
+/**document.write("<h1>Kundvagn</h1>");
+document.getElementById().innerHTML= listOfProducts; 
+document.write("<button>Lägg i varukorg</button>");
+document.write("<p>hej</p>");
+console.log(listOfProducts); 
+
+main.innerHTML = listOfProducts[0]; */
+//var mainDiv = document.getElementById('main');
+/** var index = 2;
+var aSingleProduct = listOfProducts[index];
+var image = aSingleProduct.image;
+main.innerHTML = '<img src=./assets/iPhoneX.png>';
+
+
+
+var index = 0;
+var aSingleProduct = listOfProducts[index];
+var title = aSingleProduct.title;
+ 
+console.log(title);
+document.write(title);
+
+var index = 1;
+var aSingleProduct = listOfProducts[index];
+var description = aSingleProduct.description;
+document.write(description);
+
+/**var index = 2;
+var aSingleProduct = listOfProducts[index];
+var image = aSingleProduct.image;
+document.write("<img src=./assets/iPhoneX.png>"); */
+
+
+
+
+
+
+/* Leta efter ARRAY FÖR ATT FÅ FRAM PRODUKTERNA */
 
 
 /* var ulElement =  */
@@ -34,6 +91,39 @@ main.innerHTML ="Text"
     /* Feel free to remove these other comments */
 }
 
+
+function createproductCard(listOfProducts) {
+    var productCard = document.createElement ("div")
+    productCard.classname ="allProductsClass";
+
+    
+    //Title
+    var getProductTitle = document.createElement("h2");
+    getProductTitle.innerText=listOfProducts.title;
+    productCard.appendChild(getProductTitle);
+
+    //Description
+    var getProductDescription = document.createElement("p");
+    getProductDescription.innerText=listOfProducts.description;
+    productCard.appendChild(getProductDescription);
+
+    //Image
+    var getProductImage = document.createElement("img");
+    getProductImage.src = "assets/" + listOfProducts.image;
+    productCard.appendChild(getProductImage);
+
+    //Price
+    var getProductPrice = document.createElement("h3");
+    getProductPrice.innerText=listOfProducts.price;
+    productCard.appendChild(getProductPrice);
+
+
+
+    return productCard;
+
+
+
+}
 
 /* Read the projects readme before you start! */
 /* Good luck and have fun 🤓 */
